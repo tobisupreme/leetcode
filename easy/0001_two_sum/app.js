c7c61f988441a5234843f1735c3dd00f8659dbd6
Tobi Balogun <tobi.balo@gmail.com>
2022-09-27 17:22:05 +0100

    Complete "Two Sum"

--- easy/0001_two_sum/app.js
+++ easy/0001_two_sum/app.js
@@ -3,4 +3,22 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {}
+
+/**
+ * Loop through the array nums
+ * for i = 0 until i = nums.length - 1
+ * if nums[i] + nums[i+1] == target, return [i, i+1]
+ *
+ */
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) return [i, j]
+    }
+  }
+}
+
+// Test cases
+console.log(twoSum([2, 7, 11, 15], 9))
+console.log(twoSum([3, 2, 4], 5))
+console.log(twoSum([3, 3], 6))
