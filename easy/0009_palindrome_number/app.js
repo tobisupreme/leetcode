2a812fca248cc894217f36f9c54d663a3f96e83f
Tobi Balogun <tobi.balo@gmail.com>
2022-09-30 16:49:37 +0100

    Complete "Palindrome Number"

--- easy/0009_palindrome_number/app.js
+++ easy/0009_palindrome_number/app.js
@@ -2,4 +2,35 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function (x) {}
+
+// if the number is less than zero, return false
+// initialize a variable "original" with x
+// initialize the reverse variable with 0
+// loop over the number until it is less than or equal to zero
+// Multiply the reversed variable by 10 and add the last digit of the number to it
+// remove the last digit of x
+// if the reversed number is equal to original ( initial number ), return true
+// else, false
+
+var isPalindrome = function (x) {
+  const isNegative = x < 0 ? true : false
+
+  if (isNegative) {
+    return false
+  }
+
+  const original = x
+  let reverse = 0
+
+  while (x > 0) {
+    reverse = 10 * reverse + (x % 10)
+    x = parseInt(x / 10)
+  }
+
+  return reverse == original
+}
+
+// Test cases
+console.log(isPalindrome(121))
+console.log(isPalindrome(-121))
+console.log(isPalindrome(10))
