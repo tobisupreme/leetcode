b86fe4529751276399b319bf0eb01a4c792d9ed4
Tobi Balogun <tobi.balo@gmail.com>
2022-10-16 14:46:29 +0100

    Complete - "Ransom Note" kata

--- easy/0383_ransom_note/app.js
+++ easy/0383_ransom_note/app.js
@@ -5,7 +5,7 @@
  */
 
 /**
- * Create an object, magazineCount and count all instances 
+ * Create an object, magazineCount and count all instances
  * of characters in magazine
  * For each character in ransomNote
  * If it's presence in magazineCount is greater than 0
@@ -14,4 +14,42 @@
  * return true
  */
 
-var canConstruct = function (ransomNote, magazine) {}
+var canConstruct = function (ransomNote, magazine) {
+  /**
+   * Create an object, magazineCount and count all instances
+   * of characters in magazine
+   * */
+  const magazineCount = magazine.split('').reduce((obj, letter) => {
+    if (!obj[letter]) {
+      obj[letter] = 0
+    }
+    obj[letter]++
+    return obj
+  }, {})
+
+  /**
+   * or each character in ransomNote
+   * If it's presence in magazineCount is greater than 0
+   * subtract 1 from the value of the count
+   * Else, return false
+   * return true
+   */
+  for (let i = 0; i < ransomNote.split('').length; i++) {
+    if (magazineCount[ransomNote.split('')[i]] > 0) {
+      magazineCount[ransomNote.split('')[i]]--
+    } else {
+      return false
+    }
+  }
+
+  /**
+   * Else, return false
+   * return true
+   */
+  return true
+}
+
+// Test cases
+console.log(canConstruct('a', 'b'))
+console.log(canConstruct('aa', 'ab'))
+console.log(canConstruct('aa', 'aab'))
