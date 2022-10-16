2a62326c257bf66f40034152f59bf890e67b791c
Tobi Balogun <tobi.balo@gmail.com>
2022-10-16 14:42:54 +0100

    Add pseudocode to - "Ransom Note" kata

--- easy/0383_ransom_note/app.js
+++ easy/0383_ransom_note/app.js
@@ -3,4 +3,15 @@
  * @param {string} magazine
  * @return {boolean}
  */
+
+/**
+ * Create an object, magazineCount and count all instances 
+ * of characters in magazine
+ * For each character in ransomNote
+ * If it's presence in magazineCount is greater than 0
+ * subtract 1 from the value of the count
+ * Else, return false
+ * return true
+ */
+
 var canConstruct = function (ransomNote, magazine) {}
