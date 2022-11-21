41f3b2162e4be4149419b4e505a7588a706cb579
Tobi Balogun <tobi.balo@gmail.com>
2022-11-21 21:33:07 +0100

    Add pseudocode to "Search in Rotated Sorted Array" kata

--- medium/0033_search_in_rotated_sorted_array/app.js
+++ medium/0033_search_in_rotated_sorted_array/app.js
@@ -4,4 +4,12 @@
  * @return {number}
  */
 
-var search = function (nums, target) {}
+var search = function (nums, target) {
+  /**
+   * Assign the first element as the low and the last element as the high
+   * While the low point is greater than the high point
+   *  Find mid point of the array. If target matches the mid, return the index
+   *  If target is on the left side of the array, discard the right
+   *  If target is on the right side of the array, discard the left
+   */
+}
