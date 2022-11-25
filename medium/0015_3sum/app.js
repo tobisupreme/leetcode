0d08692adc2d772288d7c27a8910cb961e429c9b
Tobi Balogun <tobi.balo@gmail.com>
2022-11-25 10:03:58 +0100

    Complete "3 Sum" kata

--- medium/0015_3sum/app.js
+++ medium/0015_3sum/app.js
@@ -2,4 +2,30 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function (nums) {}
+var threeSum = function (nums) {
+  const res = []
+  nums.sort((a, b) => a - b)
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue
+    }
+
+    let l = i + 1,
+      r = nums.length - 1
+    while (l < r) {
+      let threeSum = nums[i] + nums[l] + nums[r]
+      if (threeSum > 0) r -= 1
+      if (threeSum < 0) l += 1
+      if (threeSum === 0) {
+        res.push([nums[i], nums[l], nums[r]])
+        l += 1
+        while (nums[l] === nums[l - 1] && l < r) {
+          l += 1
+        }
+      }
+    }
+  }
+
+  return res
+}
