0ee41ff1ede5153e99540b0b29bfc5b798f7aef6
Tobi Balogun <tobi.balo@gmail.com>
2023-01-01 11:49:32 +0100

    Initial commit - "Product of Array Except Self" kata

---
+++ medium/0238__product_of_array_except_self/app.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    
+};
+
+// Test cases
+console.log(productExceptSelf([1, 2, 3, 4]))
+console.log(productExceptSelf([-1, 1, 0, -3, 3]))
