ab463791366e2f31c3e80daf7932916d6ed9e2c4
Tobi Balogun <tobi.balo@gmail.com>
2023-01-01 12:21:04 +0100

    Complete "Product of Array Except Self" kata

--- medium/0238__product_of_array_except_self/app.js
+++ medium/0238__product_of_array_except_self/app.js
@@ -2,9 +2,21 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var productExceptSelf = function(nums) {
-    
-};
+var productExceptSelf = function (nums) {
+  const output = []
+
+  for (let i = 0; i < nums.length; i++) {
+    let product = 1
+    for (let j = 0; j < nums.length; j++) {
+      if (j === i) continue
+      product *= nums[j]
+    }
+
+    output.push(product)
+  }
+
+  return output
+}
 
 // Test cases
 console.log(productExceptSelf([1, 2, 3, 4]))
