7192b34e9510beedbede8c0b2b9c43243d0705d2
Tobi Balogun <tobi.balo@gmail.com>
2023-01-03 11:08:20 +0100

    Complete "Best Time to Buy and Sell Stock" kata

--- easy/0121_best_time_to_buy_and_sell_stock/app.js
+++ easy/0121_best_time_to_buy_and_sell_stock/app.js
@@ -3,5 +3,27 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
+  let l = 0,
+    r = 1
+  let maxProfit = 0
 
-};
+  while (r < prices.length) {
+    // check if profitable
+    if (prices[l] < prices[r]) {
+      let profit = prices[r] - prices[l]
+      maxProfit = Math.max(maxProfit, profit)
+      r += 1
+    } else {
+      // update left pointer if not
+      l = r
+      r = l + 1
+    }
+  }
+
+  return maxProfit
+}
+
+// Test cases
+console.log(maxProfit([7, 1, 5, 3, 6, 4]))
+console.log(maxProfit([7, 6, 4, 3, 1]))
+console.log(maxProfit([1, 2, 4, 2, 5, 7, 2, 4, 9, 0, 9]))
