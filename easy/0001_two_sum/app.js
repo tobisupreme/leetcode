6c4d379cb1411484e95de9b15c59be8500ed165e
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2023-03-17 06:18:32 +0100

    improve: Two Sum O(n) solution

--- easy/0001_two_sum/app.js
+++ easy/0001_two_sum/app.js
@@ -10,13 +10,26 @@
  * if nums[i] + nums[i+1] == target, return [i, i+1]
  *
  */
-var twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) return [i, j]
+// var twoSum = function (nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) return [i, j]
+//     }
+//   }
+// }
+
+var twoSum = function(nums, target) {
+  let map = {};
+  for (let i= 0; i < nums.length; i++) {
+    
+    let diff = target - nums[i]
+    if (diff in map) {
+      return [map[diff], i]
     }
+
+    map[nums[i]] = i;
   }
-}
+};
 
 // Test cases
 console.log(twoSum([2, 7, 11, 15], 9))
